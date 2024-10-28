3407b8d13e7abdd6f67c6e66cd600519b61249f4
Marcus <marcus1337>
2024-10-28 15:28:03 +0100

    refactor

---
+++ src/model_yaml.py
@@ -0,0 +1,53 @@
+from bioimageio.spec.model import v0_4, v0_5
+from typing import Dict
+from conda_env import SupportedWeightsEntry
+
+class ModelYaml:
+    FORMAT_TO_WEIGHTS_ENTRY = {
+        "onnx": v0_5.OnnxWeightsDescr,
+        "pytorch_state_dict": v0_5.PytorchStateDictWeightsDescr,
+        "tensorflow_saved_model_bundle": v0_5.TensorflowSavedModelBundleWeightsDescr,
+        "torchscript": v0_5.TorchscriptWeightsDescr,
+    }
+
+    def __init__(self, model_yaml: Dict):
+        self.model_yaml = model_yaml
+
+    def _check_weights(self):
+        if not self.model_yaml.get('weights'):
+            raise ValueError("No weights found in the model YAML.")
+        
+        required_keys = ['source', 'opset_version', 'format']
+        missing_keys = [key for key in required_keys if key not in self._get_weights_entry()]
+        if missing_keys:
+            raise ValueError(f"Weight entry must contain the following keys: {', '.join(missing_keys)}")
+
+    def _check_weights_format(self):
+        weight_entry = self.model_yaml['weights'][0]
+        if weight_entry['format'] not in self.FORMAT_TO_WEIGHTS_ENTRY:
+            raise ValueError(
+                f"Unsupported format '{weight_entry['format']}' found in weight entry. "
+                f"Supported formats are: {', '.join(self.FORMAT_TO_WEIGHTS_ENTRY.keys())}"
+            )
+        
+    def _get_weights_entry(self):
+        return self.model_yaml['weights'][0]
+        
+    def _get_format_type(self):
+        return self._get_weights_entry()['format']
+        
+    def get_weights_descr_class(self):
+        return ModelYaml.FORMAT_TO_WEIGHTS_ENTRY.get(self._get_format_type())
+
+    def get_weights_descr(self) -> SupportedWeightsEntry:
+        weight_entry = self._get_weights_entry()
+        weights_descr_class = self.get_weights_descr_class()
+        descr = weights_descr_class(
+            opset_version=weight_entry.get("opset_version"),
+            source=weight_entry.get("source") 
+        )
+        return descr
+
+    def validate(self):
+        self._check_weights()
+        self._check_weights_format()
--- src/test_conda_env.py
+++ src/test_conda_env.py
@@ -1,29 +1,13 @@
 import inspect
 import yaml
 from pathlib import Path
-from conda_env import get_conda_env, SupportedWeightsEntry
-from bioimageio.spec.model import v0_4, v0_5
-
+from conda_env import get_conda_env
+from model_yaml import ModelYaml
 
 def _get_model_yaml():
     model_yaml_file_path = Path(__file__).parent / "test_model.yml"
     with model_yaml_file_path.open('r') as file:
         return yaml.safe_load(file)
-    
-def _get_weights_entry(model_yaml: dict) -> SupportedWeightsEntry:
-    if not model_yaml.get('weights'):
-        raise ValueError("No weights found in the model YAML.")
-    
-    weight_entry = model_yaml['weights'][0]
-    
-    if 'source' not in weight_entry or 'opset_version' not in weight_entry:
-        raise ValueError("Weight entry must contain 'source' and 'opset_version'.")
-   
-    entry = v0_4.OnnxWeightsDescr(
-        opset_version=weight_entry.get("opset_version"),
-        source=weight_entry.get("source") 
-    )
-    return entry
 
 """
 Given a model yaml file, resolve it to a conda file and build an environment.
@@ -31,6 +15,8 @@ Given a model yaml file, resolve it to a conda file and build an environment.
 def test_conda_env_creation():
     print(f"Running {inspect.currentframe().f_code.co_name}")
     model_yaml = _get_model_yaml()
-    conda_env = get_conda_env(env_name="test_env", entry=_get_weights_entry(model_yaml))
+    model_yaml_obj = ModelYaml(model_yaml)
+    model_yaml_obj.validate()
+    conda_env = get_conda_env(env_name="test_env", entry=model_yaml_obj.get_weights_descr())
     print(conda_env)
     
