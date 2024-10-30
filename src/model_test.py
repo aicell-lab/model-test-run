cc4c46339da58eed48e344f67bf61fb6555493f3
Marcus <marcus1337>
2024-10-30 14:34:34 +0100

    failing download test

--- src/model_test.py
+++ src/model_test.py
@@ -2,9 +2,15 @@ import yaml
 from model_yaml import ModelYaml
 from bioimageio.core import test_model
 from bioimageio.spec.summary import ValidationSummary
+from model_download import ModelDownloader
 
 def run_model_tests(model_yaml: yaml) -> ValidationSummary:
     my = ModelYaml(model_yaml)
     source = my.get_weights_source()
     weight_format = my.get_weights_format()
-    return test_model(source=source, weight_format=weight_format)
+
+    downloader = ModelDownloader(source)
+    downloader.download_model()
+    model_filepath = str(downloader.get_download_filepath())
+
+    return test_model(source=model_filepath, weight_format=weight_format)
