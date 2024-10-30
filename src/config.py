acb2d27e9b93fd0f5d58e700e4d78a36bc933dc1
Marcus <marcus1337>
2024-10-30 10:34:12 +0100

    rm unused files

--- src/config.py
+++ src/config.py
@@ -10,8 +10,6 @@ class Config:
         client_id = "tester"
         client_name = "MTest"
         TOKEN_VAR_NAME = "MTEST_SERVICE_TOKEN"
-    class Scripts:
-        conda_pack_path = Path(__file__).parent.parent / "scripts" / "pack_conda_env.sh"
     class Storage:
         tmp_dir = Path(tempfile.gettempdir())
     UNKNOWN_NAME = "N/A"
