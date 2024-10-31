a8e593c213800d72121e9c946cabe454cc49703a
Marcus <marcus1337>
2024-10-31 14:14:08 +0100

    refactor

--- src/packing/conda_env_controller.py
+++ src/packing/conda_env_controller.py
@@ -5,57 +5,73 @@ from typing import Dict
 from packing.conda_env import get_conda_env, CondaEnv
 from data.model_value_converter import ModelValueConverter
 from data.model_values import ModelValues
+from pathlib import Path
 
-class CondaEnvController:
+class CondaFileHandler:
     def __init__(self, model_yaml: Dict):
         self.values = ModelValues.from_dict(model_yaml)
         self.model_yaml = model_yaml
-        self._set_paths()
-    
-    def _set_paths(self):
-        self.tmp_model_yaml_filepath = Config.Storage.tmp_dir / f"{self.values.name}.yml"
-        self.tmp_env_deps_yaml_path = Config.Storage.tmp_dir / f"{self.values.name}_deps.yml"
-
-    def _dump_model_yaml(self):
-        with open(self.tmp_model_yaml_filepath, 'w') as file:
-            yaml.dump(self.model_yaml, file)
-
-    def create_conda_env(self):
-        self._dump_model_yaml()
-        print(f"Creating conda environment from {self.tmp_model_yaml_filepath}...")
-        subprocess.run(
-            ["conda", "env", "create", "-f", str(self.tmp_model_yaml_filepath), "-n", self.values.name],
-            check=True
-        )
-
-    def remove_conda_env(self):
-        print(f"Removing existing conda environment '{self.values.name}' if it exists...")
-        try:
-            subprocess.run(
-                ["conda", "env", "remove", "--name", self.values.name, "--yes"],
-                check=True
-            )
-            print(f"Removed existing conda environment '{self.values.name}'.")
-        except subprocess.CalledProcessError:
-            print(f"No existing environment '{self.values.name}' found.")
 
     def _get_conda_env(self) -> CondaEnv:
         weights_descr = ModelValueConverter(self.model_yaml).get_weights_descr()
         return get_conda_env(env_name=self.values.name, entry=weights_descr)  
     
-    def _dump_dependencies_yaml(self):
+    def get_dependencies_path(self) -> Path:
+        return Config.Storage.tmp_dir / f"{self.values.name}_deps.yml"
+
+    def get_model_yaml_path(self) -> Path:
+        return Config.Storage.tmp_dir / f"{self.values.name}.yml"
+
+    def dump_dependencies_yaml(self):
         env_dependencies = self._get_conda_env()
-        with open(self.tmp_env_deps_yaml_path, 'w') as file:
+        with open(self.get_dependencies_path(), 'w') as file:
             yaml.dump(env_dependencies, file)
 
-    def install_dependencies(self):
-        self._dump_dependencies_yaml()
-        print(f"Installing dependencies for conda environment '{self.values.name}'...")
-        try:
-            subprocess.run(
-                ["conda", "env", "update", "--file", str(self.tmp_env_deps_yaml_path), "--name", self.values.name],
+    def dump_model_yaml(self):
+        with open(self.get_model_yaml_path(), 'w') as file:
+            yaml.dump(self.model_yaml, file)
+
+class CondaEnvController:
+    def __init__(self, model_yaml: Dict):
+        self.values = ModelValues.from_dict(model_yaml)
+        self.file_handler = CondaFileHandler(model_yaml)
+    
+    def run_create_env_process(self):
+        subprocess.run(
+            ["conda", "env", "create", "-f", str(self.file_handler.get_model_yaml_path()), "-n", self.values.name],
+            check=True
+        )
+    
+    def run_remove_env_process(self):
+        subprocess.run(
+                ["conda", "env", "remove", "--name", self.values.name, "--yes"],
                 check=True
             )
+    
+    def run_update_env_process(self):
+        subprocess.run(
+                ["conda", "env", "update", "--file", str(self.file_handler.get_dependencies_path()), "--name", self.values.name],
+                check=True
+            )
+
+    def create_conda_env(self):
+        self.file_handler.dump_model_yaml()
+        print(f"Creating conda environment from {self.file_handler.get_model_yaml_path()}...")
+        self.run_create_env_process()
+
+    def remove_conda_env(self):
+        print(f"Removing existing conda environment '{self.values.name}' if it exists...")
+        try:
+            self.run_remove_env_process()
+            print(f"Removed existing conda environment '{self.values.name}'.")
+        except subprocess.CalledProcessError:
+            print(f"No existing environment '{self.values.name}' found.")
+
+    def install_dependencies(self):
+        self.file_handler.dump_dependencies_yaml()
+        print(f"Installing dependencies for conda environment '{self.values.name}'...")
+        try:
+            self.run_update_env_process()
             print(f"Dependencies installed successfully in '{self.values.name}'.")
         except subprocess.CalledProcessError as e:
             print(f"Error occurred while installing dependencies: {e}")
