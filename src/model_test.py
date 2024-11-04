2c5be80de40dc0b8eb6e917545b82b3aca36245b
Marcus <marcus1337>
2024-11-04 13:24:10 +0100

    change result print format

--- src/model_test.py
+++ src/model_test.py
@@ -2,9 +2,22 @@ from bioimageio.core import test_model
 from bioimageio.spec.summary import ValidationSummary
 
 def _print_result(r: ValidationSummary):
-    print(f"Model '{r.name}': {r.status}")
+    passed_details = []
+    failed_details = []
+    
     for detail in r.details:
-        print(f"{detail.name}: {detail.status}")
+        if detail.status == 'passed':
+            passed_details.append(detail.name)
+        else:
+            failed_details.append(detail.name)
+    
+    print(f"Model '{r.name}': {r.status}")
+    print("passed:")
+    for name in passed_details:
+        print(f"  {name}")
+    print("failed:")
+    for name in failed_details:
+        print(f"  {name}")
 
 def run_model_tests(rdf_yaml_path) -> bool:
     result = test_model(source=rdf_yaml_path)
