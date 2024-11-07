8a72e6ad1169b16411b976e36dfa1cddbe33a794
Marcus <marcus1337>
2024-11-07 15:05:09 +0100

    refactor & simplify checks

--- src/data/model_value_converter.py
+++ src/data/model_value_converter.py
@@ -1,8 +1,8 @@
 from bioimageio.spec.model import v0_5
 from packing.conda_env import SupportedWeightsEntry
-from typing import Dict
+from typing import Dict, List
 from data.model_values import ModelValues, ModelWeights
-from data.record_files import RecordFileHandler
+from pydantic import HttpUrl
 
 class ModelValueConverter:
     FORMAT_TO_WEIGHTS_ENTRY = {
@@ -14,9 +14,13 @@ class ModelValueConverter:
 
     def __init__(self, model_yaml: Dict):
         self.values = ModelValues.from_dict(model_yaml)
-        self.record_files_handler = RecordFileHandler(model_yaml)
-        self.record_files_handler.download_and_extract_files()
         self.w_index = 0 # Currently only tests the 1st weight entry
+
+    def _get_weight_sources(self) -> List[str]:
+        return [weight.source for weight in self.values.weights]
+    
+    def _get_weight_source_urls(self) -> List[HttpUrl]:
+        return [f"https://zenodo.org/records/{self.values.zenodo.revision_id}/files/{source}" for source in self._get_weight_sources()]
     
     def _get_weight(self) -> ModelWeights:
         return self.values.weights[self.w_index]
@@ -31,7 +35,7 @@ class ModelValueConverter:
         }
         return self.get_weights_descr_class()(
             **version_info,
-            source=self.record_files_handler.get_weights_source_paths()[self.w_index]
+            source=self._get_weight_source_urls()[self.w_index]
         )
 
     
--- src/model_test.py
+++ src/model_test.py
@@ -2,22 +2,20 @@ from bioimageio.core import test_model
 from bioimageio.spec.summary import ValidationSummary
 
 def _print_result(r: ValidationSummary):
-    passed_details = []
-    failed_details = []
-    
-    for detail in r.details:
-        if detail.status == 'passed':
-            passed_details.append(detail.name)
-        else:
-            failed_details.append(detail.name)
+    passed_details = [detail.name for detail in r.details if detail.status == 'passed']
+    failed_details = [detail.name for detail in r.details if detail.status != 'passed']
     
     print(f"Model '{r.name}': {r.status}")
-    print("passed:")
-    for name in passed_details:
-        print(f"  {name}")
-    print("failed:")
-    for name in failed_details:
-        print(f"  {name}")
+    
+    if passed_details:
+        print(f"{len(passed_details)} passed tests:")
+        for name in passed_details:
+            print(f"  {name}")
+    
+    if failed_details:
+        print(f"{len(failed_details)} failed tests:")
+        for name in failed_details:
+            print(f"  {name}")
 
 def run_model_tests(rdf_yaml_path) -> bool:
     result = test_model(source=rdf_yaml_path)
